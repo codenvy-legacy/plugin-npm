fea8b400e4c95beff2757d06b9dac360261dd0bf
Florent Benoit <Florent.Benoit@serli.com>
2014-10-03 10:44:56 +0200

    PLGAJS-88: The npm dependencies are installed by the runner so we don't need to add them into the user project

--- client/src/main/java/com/codenvy/plugin/npm/client/NpmExtension.java
+++ client/src/main/java/com/codenvy/plugin/npm/client/NpmExtension.java
@@ -11,17 +11,12 @@
 package com.codenvy.plugin.npm.client;
 
 import com.codenvy.ide.api.action.ActionManager;
-import com.codenvy.ide.api.constraints.Constraints;
-import com.codenvy.ide.api.action.DefaultActionGroup;
 import com.codenvy.ide.api.extension.Extension;
 import com.codenvy.ide.extension.builder.client.BuilderLocalizationConstant;
 import com.codenvy.plugin.npm.client.menu.LocalizationConstant;
 import com.codenvy.plugin.npm.client.menu.NpmInstallAction;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-
-import static com.codenvy.ide.api.constraints.Anchor.AFTER;
-import static com.codenvy.ide.api.action.IdeActions.GROUP_BUILD;
 
 /**
  * Extension registering NPM commands
@@ -40,14 +35,17 @@ public class NpmExtension {
         // Register action
         actionManager.registerAction(localizationConstantNpm.npmInstallId(), npmInstallAction);
 
+
+        // For now the NPM dependencies are installed by the runner
+
         // Get Build menu
-        DefaultActionGroup buildMenuActionGroup = (DefaultActionGroup)actionManager.getAction(GROUP_BUILD);
+        //DefaultActionGroup buildMenuActionGroup = (DefaultActionGroup)actionManager.getAction(GROUP_BUILD);
 
         // create constraint
-        Constraints afterBuildConstraints = new Constraints(AFTER, builderLocalizationConstant.buildProjectControlId());
+        //Constraints afterBuildConstraints = new Constraints(AFTER, builderLocalizationConstant.buildProjectControlId());
 
         // Add NPM in build menu
-        buildMenuActionGroup.add(npmInstallAction, afterBuildConstraints);
+        //buildMenuActionGroup.add(npmInstallAction, afterBuildConstraints);
 
     }
 }
